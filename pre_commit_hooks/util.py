2d05726c009c0e78d01e5577c92688cbba4d2f32
Eike Schumann <eike.schumann@tba.group>
2017-10-26 19:20:20 +0200

    NExt test

--- pre_commit_hooks/util.py
+++ pre_commit_hooks/util.py
@@ -28,7 +28,9 @@ def cmd_output(*cmd, **kwargs):
    retcode = kwargs.pop('retcode', 0)
    popen_kwargs = {'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE}
    popen_kwargs.update(kwargs)
+   print( 'Command: {}'.format(cmd) )
    proc = subprocess.Popen(cmd, **popen_kwargs)
+   print( 'Proc: {}'.format(proc) )
    stdout, stderr = proc.communicate()
    stdout = stdout.decode('UTF-8')
    if stderr is not None:
