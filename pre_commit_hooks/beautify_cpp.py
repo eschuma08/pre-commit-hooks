f03a0e6319bb850453774c76026f33c47ab470d7
Eike Schumann <eike.schumann@tba.group>
2017-10-26 19:38:57 +0200

    NExt test

--- pre_commit_hooks/beautify_cpp.py
+++ pre_commit_hooks/beautify_cpp.py
@@ -8,7 +8,7 @@ def check( filenames ):
    #files_to_check = set([ str( os.path.abspath( filename )) for filename in filenames ]) + (added_files)
    files_to_check = added_files() # & set(filenames) 
    
-   print ('files_to_check: {}'.format(files_to_check) )
+   #print ('files_to_check: {}'.format(files_to_check) )
    script_path = os.path.realpath(__file__)
    bin_bath = os.path.join( os.path.dirname( os.path.abspath(script_path) ), '..', 'bin' )
    #cfg_file = os.path.join( bin_bath, 'defaults.cfg' )
@@ -24,16 +24,20 @@ def check( filenames ):
    
    
    #format_cpp( files_to_check, os.path.join( bin_bath, 'uncrustify.exe'), ['-c', '{}'.format(cfg_file), '*.cpp' ] )
+   retval = 0
    exe = os.path.join( bin_bath, 'AStyle.exe')
    args = [str(exe), '--style=google', '--indent=spaces',  '--max-code-length=80'] + [ file for file in files_to_check ]
    try:
 #      ret = cmd_output( str(exe), '--style=google', '--indent=spaces', '--delete-empty-lines', '--pad-oper', '--pad-comma', '--pad-paren', '--pad-header',  '--max-code-length=80', '--close-templates', '--break-after-logical', '--lineend=linux', '--suffix=none', ' '.join( files_to_check )  )
-      ret = cmd_output( *args )
-      print( 'Return value = {}',format(ret) )
+      ret = cmd_output( *args ).splitlines()
+      for line in ret:
+         if 'Formatted' in line:
+            retval = 1
+      #print( 'Return value = {}',format(ret) )
    except CalledProcessError:
       pass
 
-   return 0
+   return retval
 
 
 def main(argv=None):
