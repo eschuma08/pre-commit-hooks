c5d1eccc09b02bf13abf53f8c6cb9662e15392fe
Eike Schumann <eike.schumann@tba.group>
2017-10-26 19:11:24 +0200

    NExt test

--- pre_commit_hooks/beautify_cpp.py
+++ pre_commit_hooks/beautify_cpp.py
@@ -23,7 +23,7 @@ def check( filenames ):
    
    #format_cpp( files_to_check, os.path.join( bin_bath, 'uncrustify.exe'), ['-c', '{}'.format(cfg_file), '*.cpp' ] )
    exe = os.path.join( bin_bath, 'AStyle.exe')
-   args = set([str(exe), '--style=google', '--indent=spaces',  '--max-code-length=80']) & files_to_check
+   args = [str(exe), '--style=google', '--indent=spaces',  '--max-code-length=80'] + [ file for file in files_to_check ]
    try:
 #      ret = cmd_output( str(exe), '--style=google', '--indent=spaces', '--delete-empty-lines', '--pad-oper', '--pad-comma', '--pad-paren', '--pad-header',  '--max-code-length=80', '--close-templates', '--break-after-logical', '--lineend=linux', '--suffix=none', ' '.join( files_to_check )  )
       ret = cmd_output( *args )
